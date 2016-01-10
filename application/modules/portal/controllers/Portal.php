e082d5b6e0597c20a5b04ba2c75f6b68ee8e0c95
Max <Max>
2016-01-10 17:33:14 -0500

    initial input

---
+++ application/modules/portal/controllers/Portal.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Portal extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(array('ion_auth','form_validation'));
+		$this->load->helper(array('url','language'));
+
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		$this->lang->load('auth');
+	}
+
+	function index()
+	{
+		if(!$this->ion_auth->logged_in())
+			$this->redirect("/auth/");
+		$this->load->view("portal/home");
+	}
+}
